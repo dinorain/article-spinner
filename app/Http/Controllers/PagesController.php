a5760fdced3d858ff90fa2c583679175ea3603a6
Dustin Jourdan <djourdan555@gmail.com>
2019-10-12 22:30:28 +0700

    passing variables

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,10 +7,18 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index() {
-        return view('pages.index');
+        $title = 'Welcome to Laravel';
+        //return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title); // passing single variable
+
     }
 
     public function services() {
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web', 'Mobile']
+
+        );
+        return view('pages.services')->with($data); // passing multi variables
     }
 }
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -4,8 +4,15 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-                Services
+                {{$title}}
             </div>
+            @if(count($services)>0)
+                <ul>
+                    @foreach($services as $service)
+                        <li>{{$service}} </li>
+                    @endforeach
+                </ul>
+            @endif
         </div>
     </div>
 @endsection
