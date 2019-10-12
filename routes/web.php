d52be201e12f4f8d78122bafae932f59d606e2e0
Dustin Jourdan <djourdan555@gmail.com>
2019-10-12 21:41:58 +0700

    made mw first page here

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return '<h1>Hello World</h1>';
+});
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
